cbeeb380819f3b02de04e34cbff86720a7e2748c
YusufGkl <yusufgkl@gmail.com>
2017-06-08 21:25:07 +0200

    that dank commit

---
+++ gender.py
@@ -0,0 +1,22 @@
+from sklearn import tree
+
+#input from user 
+h = raw_input("enter height: ")
+w = raw_input("enter weight: ")
+s = raw_input("enter shoe size: ")
+
+#dataset
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+  [190, 90, 47], [175, 64, 39],
+  [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
+  'female', 'male', 'male']
+
+clf = tree.DecisionTreeClassifier()
+
+clf = clf.fit(X,Y)
+
+prediction = clf.predict([[h,w,s]])
+
+print prediction
